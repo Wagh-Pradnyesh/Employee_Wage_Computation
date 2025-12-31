9104661ef7fc147b7686650f6f64158e5cb0bcee
Wagh-Pradnyesh <pradnyeshwagh@gmail.com>
2025-12-31 16:19:24 +0530

    [Modified] UC-1-Complete

--- src/com/infogalaxy/employeewagecomp/EmployeeWageComp.java
+++ src/com/infogalaxy/employeewagecomp/EmployeeWageComp.java
@@ -1,10 +1,26 @@
 package com.infogalaxy.employeewagecomp;
 
+import java.util.Scanner;
+
 public class EmployeeWageComp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int attendance = 0;
+		Scanner scanner = new Scanner(System.in);
+		
 		System.out.println("Welcome to Employee Wage Computation Program");
+		
+		System.out.println("Enter The Attendance :[Absent=0/Present=1]:");
+		attendance=scanner.nextInt();
+		if (attendance==0) {
+			System.out.println("Employee Is Absent");
+		}
+		else {
+			System.out.println("Employee Is Present");
+		}
+		
+		
 	}
 
 }
